20644d676d82175823591a86bd7b816ff65c8538
Alex Kluew <dropbyalex@gmail.com>
2018-12-06 12:59:39 -0500

    (chore) more work on trying to get coveralls to work

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   testPathIgnorePatterns: [
-    '<rootDir>/.next/',
     '<rootDir>/node_modules/',
-    '<rootDir>/.vscode/',
     '<rootDir>/routes/redirector'
   ],
   bail: true,
-  verbose: true
+  verbose: true,
+  collectCoverage: true
 };
